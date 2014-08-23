48a6d61b39f85a25464b470194c2373cebf1d721
Todd Eichel <todd@toddeichel.com>
2014-08-23 15:51:46 -0700

    Remove deploy shell script; replaced by Rakefile.

--- _deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-s3cmd sync _site/ s3://toddeichel.com/ \
-  --cf-invalidate \
-  --delete-removed \
-  --acl-public \
-  --verbose \
-  --exclude 'deploy.sh' \
-  # --dry-run
-  # --reduced-redundancy \
